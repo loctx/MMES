f4ae9c08a2939280bfcd8312adc56c9441b1bedd
DESKTOP-OCQ9C2M\HP <toi.la.dan.xu.nghe@gmail.com>
2023-06-20 16:50:17 +0700

    Add controller Stone

--- PROJECT.BE/PROJECT.Core/AppDbContext.cs
+++ PROJECT.BE/PROJECT.Core/AppDbContext.cs
@@ -100,6 +100,7 @@ namespace PROJECT.CORE
         public DbSet<tblMdPartner> tblMdPartner { get; set; }
         public DbSet<tblMdPourType> tblMdPourType { get; set; }
         public DbSet<tblMdOrderType> tblMdOrderType { get; set; }
+        public DbSet<tblMdStone> tblMdStone { get; set; }
 
         #endregion
 
